bb584dc55d574e440f56e3a6223ceac358ba389a
Gchevanel <gchevanel@hotmail.com>
2019-03-05 19:00:51 -0500

    linked page work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
     def index
     end
+
+    def residential
+    end
+
+    def corporate
+    end
+
+    def quote
+    end
+    
+    def thanks
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#index'
+  get 'index' => 'pages#index'
+  get 'residential' => 'pages#residential'
+  get 'corporate' => 'pages#corporate'
+  get 'quote' => 'pages#quote'
+  get 'thanks' => 'pages#thanks'
 
 end
